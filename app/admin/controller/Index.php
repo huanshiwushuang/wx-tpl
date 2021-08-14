da807cbda38e11ac2509b2adee0cd4c2f2ab7221
guohao <gh9694@foxmail.com>
2021-08-14 22:50:39 +0800

    勉强把vue3-sfc-loader跑通了，睡觉

--- app/admin/controller/Index.php
+++ app/admin/controller/Index.php
@@ -8,10 +8,24 @@ class Index extends Base
 {
     public function index()
     {
-        return '我是 index';
+        $tpl_var = [
+            'T' => '后台首页-戊戌数据',
+            'K' => '后台首页-我是keywords',
+            'D' => '后台首页-我是description',
+        ];
+
+        // fetch 数据页面
+        return View::fetch('index/index', $tpl_var);
     }
     public function about()
     {
-        return '我是 about';
+        $tpl_var = [
+            'T' => '后台-关于我们-戊戌数据',
+            'K' => '后台-关于我们-我是keywords',
+            'D' => '后台-关于我们-我是description',
+        ];
+
+        // fetch 数据页面
+        return View::fetch('index/about', $tpl_var);
     }
 }
