479a0d7b4f147becc30ceefde9e46b205af7b4ec
guohao <gh9694@foxmail.com>
2021-08-17 19:02:53 +0800

    优化-优化优化

--- public/static/admin/main.js
+++ public/static/admin/main.js
@@ -26,7 +26,7 @@ require.config({
 
 require(['init'], Init => {
 
-	require(['vue', 'vue_loader', 'router/mode', 'router/index'], function (Vue, VueLoader, RouterMode, router) {
+	require(['vue', 'router/mode', 'router/index'], function (Vue, RouterMode, router) {
 		(async () => {
 			// 配置路由模式
 			RouterMode.config({
@@ -56,50 +56,3 @@ require(['init'], Init => {
 	})
 	
 })
-
-// import Init from './init';
-// import Vue from 'vue'
-// import App from './App.vue'
-// // 路由模式
-// import RouterMode from './router/mode'
-// import router from './router'
-
-// Vue.config.productionTip = false
-
-// // 配置路由模式
-// RouterMode.config({
-// 	mode: 'ast',
-// });
-
-// // 混入可观测的数据
-// Vue.mixin({
-// 	data() {
-// 		return Init.mixinData
-// 	}
-// })
-// // 附加其他数据
-// Object.assign(Vue.prototype, Init.protoData);
-
-// // 批量注册基础组件
-// // ??????
-
-
-// const app = new Vue({
-// 	router,
-// 	render: h => h(App)
-// }).$mount('#app');
-
-// // 生产环境，删除一些DOM
-// if (process.env.NODE_ENV === 'production') {
-// 	var n = document.querySelectorAll(".data,.node_remove");
-// 	for (var i = 0; i < n.length; i++) {
-// 		n[i].parentNode.removeChild(n[i]);
-// 	}
-// }
-
-// if (process.env.NODE_ENV === "development") {
-// 	window.app = app;
-// } else {
-// 	window.krk7jl7x = app;
-// }
-
