4b0658d0be4c570a85617382d75f2bcd8d9ce509
guohao <gh9694@foxmail.com>
2021-10-20 16:08:53 +0800

    优化-优化优化

--- app/admin/controller/Base.php
+++ app/admin/controller/Base.php
@@ -2,96 +2,37 @@
 
 namespace app\admin\controller;
 
-use app\BaseController;
-use think\facade\View;
-use kqufcgta_html5\WxHTML5;
+use app\WxBaseController;
 
-class Base extends BaseController
+class Base extends WxBaseController
 {
-
-	protected function initialize()
+	public function initialize()
 	{
-		// 过滤所有最终输出的页面
-		$this->filterAllView();
+		$this->filter_view();
 	}
 
-	// 模板输出统一过滤器
-	protected function filterAllView()
+	// 子应用-模板输出统一过滤器
+	protected function filter_view()
 	{
-		View::filter(function ($content) {
-			// 获取模板变量
-			$tpl_var = View::instance()->get();
+		array_push($this->start_tag_call, function (&$name, &$attributes = [], &$selfClosing = false) {
+			// 修改 a 标签
+			if (preg_match('/^a$/i', $name)) {
+				// 默认所有链接都不是白名单
+				$is_whitelist = false;
 
-			// 不是 entry 页面，则再 fetch entry 页面, $content作为 krt18uif_data ，塞进去。
-			if (!array_key_exists('krxg93vb_is_entry', $tpl_var) || !$tpl_var['krxg93vb_is_entry']) {
-				
-				// 数据源
-				$tpl_var['krt18uif_data'] = $content;
+				$url = trim($attributes['href']);
 
-				// 入口页面标识变量
-				$tpl_var['krxg93vb_is_entry'] = true;
+				// 戊戌数据
+				if (preg_match('/wuxuwang\.com/', $url) || preg_match('/^\//', $url)) {
+					$is_whitelist = true;
+				}
 
-				// 变量检查
-				$tpl_var = $this->tpl_var_check($tpl_var);
-
-				return View::fetch('main/entry', $tpl_var);
+				if ($is_whitelist) {
+					unset($attributes['rel']);
+				} else {
+					$attributes['rel'] = 'nofollow';
+				}
 			}
-
-			// 最后一次的 fetch，则直接输出
-			$html5 = new WxHTML5();
-
-			$dom = $html5->loadHTML($content, [
-				// 去除源码注释
-				'remove_comment' => true,
-				// 是否压缩 源码
-				'minify_source' => true,
-				// 如果 dom 属性中的 key 或者 value 中，有数组中的任一关键字，则此 DOM 中的数据将不会被压缩
-				'any_not_minify' => [
-					'kreq0txx_not_minify'
-				],
-				// 标签开始
-				'onStartTag' => function (&$name, &$attributes = array(), &$selfClosing = false) {
-					// 修改 a 标签
-					if (preg_match('/^a$/i', $name)) {
-						// 默认所有链接都不是白名单
-						$is_whitelist = false;
-
-						$url = trim($attributes['href']);
-
-						// 戊戌数据
-						if (preg_match('/wuxuwang\.com/', $url) || preg_match('/^\//', $url)) {
-							$is_whitelist = true;
-						}
-
-						if ($is_whitelist) {
-							unset($attributes['rel']);
-						} else {
-							$attributes['rel'] = 'nofollow noopener noreferrer';
-						}
-					}
-				},
-			]);
-
-			return $html5->saveHTML($dom);
 		});
-
-	}
-	// 变量检查，比如公有变量必须有默认值
-	protected function tpl_var_check($tpl_var) {
-		// 标题
-		if (!isset($tpl_var['T'])) {
-			$tpl_var['T'] = '戊戌数据 - 医药（数据）共享家';
-		}
-		// 关键词
-		if (!isset($tpl_var['K'])) {
-			$tpl_var['K'] = '戊戌数据,医药数据库,药品查询,药品库,药物杂质对照品库,一致性评价库,医保目录,临床试验库,药物目录,制药企业,医药公司,医药行业数据,戊戌网,戊戌数据官网';
-		}
-		// 描述
-		if (!isset($tpl_var['D'])) {
-			$tpl_var['D'] = '戊戌数据是以医药数据为核心，集数据、资讯、政策于一体的信息服务平台，国内第一家信息开放、共享的医药数据库。致力于把数据信息带入每个企业，推动企业研发智能化、市场营销决策化，构建信息互联的医药新世界。医药数据的更多信息就在戊戌数据官网。';
-		}
-
-		
-		return $tpl_var;
 	}
 }
--- public/static/admin/views/about.vue
+++ public/static/admin/views/about.vue
@@ -1,16 +1,11 @@
 <template>
-  <div class="about">
-    <h1>
-		{{ ast.krkhqpa2_about }}
-	</h1>
+    <div class="about">
+        <h1>
+            {{ ast.krkhqpa2_about }}
+        </h1>
 
-	<router-link to="/">
-		回到首页
-
-	</router-link>
-	<iframe class="sjc" src="https://view.officeapps.live.com/op/view.aspx?src=http%3A%2F%2Fdemo.dzz.cc%2Findex.php%3Fmod%3Dio%26op%3DgetStream%26path%3DVlR3VHA3REJQT1lFWml2TmNVMWZQS0xhZVU4d0hZOFFhVHhNcl9NRGxSSlpiZkRMRGJTb09Bc3JFQVgyN2dVazJSR05PclZuS1cxOEVn%26n%3D13.pptx%26t%3D1632842168">
-	</iframe>
-  </div>
+        <router-link to="/"> 回到首页 </router-link>
+    </div>
 </template>
 
 <script>
@@ -21,7 +16,7 @@ module.exports = {
 
 <style lang="less">
 .sjc {
-	width: 100vw;
-	height: 100vh;
+    width: 100vw;
+    height: 100vh;
 }
 </style>
