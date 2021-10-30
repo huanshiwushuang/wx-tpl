dafc04c1ae8a280c567cff05bf91a4aa96865286
guohao <gh9694@foxmail.com>
2021-10-31 00:13:33 +0800

    优化-优化优化

--- public/static/defaultApp/development/js/include_head.js
+++ public/static/defaultApp/development/js/include_head.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "22a91d6a0d44695a58e8";
+/******/ 	var hotCurrentHash = "87c6269f6a778fdeed90";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
