c1d74f666a0fff137f13e98a3c96fbe82a7bec40
guohao <gh9694@foxmail.com>
2021-11-15 18:24:24 +0800

    优化-优化优化

---
+++ wx-tpl-front/src/views/PC.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        123
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PC",
+};
+</script>
+
+<style lang="less">
+@import (reference) "@/assets/less/index.less";
+</style>
