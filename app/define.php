7c0ed3aa58f268ed0e76b5a1592d8221e6113d15
guohao <gh9694@foxmail.com>
2021-11-18 11:46:59 +0800

    优化-优化优化

--- app/define.php
+++ app/define.php
@@ -5,5 +5,7 @@
 define('DS', DIRECTORY_SEPARATOR);
 define('PROJECT_ROOT', __DIR__ . DS . '..');
 define('APP_ROOT', PROJECT_ROOT . DS . 'app');
-define('ENV', env('env') ?? 'production');
 
+// domain > .env
+$env = preg_match('/juzimi\.online/i', $_SERVER['HTTP_HOST']) ? 'production' : env('env') ?? 'development';
+define('ENV', $env);
