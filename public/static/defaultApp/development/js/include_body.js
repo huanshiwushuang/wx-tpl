6af59c0a04c921c63f232066371038fe8012745e
guohao <gh9694@foxmail.com>
2021-11-23 09:43:20 +0800

    编译-编译编译

--- public/static/defaultApp/development/js/include_body.js
+++ public/static/defaultApp/development/js/include_body.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a78046d9e9693618e703";
+/******/ 	var hotCurrentHash = "321a8ab727bb3aa05e33";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
