947c954009e29a07c1ff62d22b47058da80e218e
guohao <gh9694@foxmail.com>
2021-11-27 00:03:08 +0800

    优化-优化优化

---
+++ public/static/admin/lib/module_httpVueLoader/index.js
@@ -0,0 +1,16 @@
+// require config
+require.config({
+    paths: {
+        vue: 'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js?noext',
+        lodash: 'https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js?noext',
+        axios: 'https://cdn.jsdelivr.net/npm/axios@0.24.0/dist/axios.min.js?noext',
+        vuex: 'https://cdn.jsdelivr.net/npm/vuex@3.6.2/dist/vuex.min.js?noext',
+        // *************************************************************************************
+        '@vue/composition-api': 'https://cdn.jsdelivr.net/npm/@vue/composition-api@1.0.5?noext',
+        echarts: 'https://cdn.jsdelivr.net/npm/echarts@5.1.2?noext',
+        'vue-echarts': 'https://cdn.jsdelivr.net/npm/vue-echarts@6.0.0?noext',
+    },
+    shim: {
+
+    }
+})
