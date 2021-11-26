bebf3d0986cda9b6659263092270f7a046b34ffd
guohao <gh9694@foxmail.com>
2021-11-26 21:27:02 +0800

    优化-优化优化

--- app/admin/controller/Base.php
+++ app/admin/controller/Base.php
@@ -14,25 +14,6 @@ class Base extends WxBaseController
 	// 子应用-模板输出统一过滤器
 	protected function filter_view()
 	{
-		array_push($this->start_tag_call, function (&$name, &$attributes = [], &$selfClosing = false) {
-			// 修改 a 标签
-			if (preg_match('/^a$/i', $name)) {
-				// 默认所有链接都不是白名单
-				$is_whitelist = false;
-
-				$url = trim($attributes['href']);
-
-				// 戊戌数据
-				if (preg_match('/wuxuwang\.com/', $url) || preg_match('/^\//', $url)) {
-					$is_whitelist = true;
-				}
-
-				if ($is_whitelist) {
-					unset($attributes['rel']);
-				} else {
-					$attributes['rel'] = 'nofollow';
-				}
-			}
-		});
+		
 	}
 }
--- public/static/admin/views/home.vue
+++ public/static/admin/views/home.vue
@@ -1,35 +1,47 @@
 <template>
     <div class="home">
-        <a href="https://www.wuxuwang.com" target="_blank">
-            <img alt="Vue logo" src="/static/admin/assets/img/krt3th48.jpg" />
+        <a
+            href="https://www.wuxuwang.com"
+            target="_blank"
+        >
+            <img
+                alt="Vue logo"
+                src="/static/admin/assets/img/krt3th48.jpg"
+            />
         </a>
 
         <ul class="kseoxuv4_ul">
-			<li>
-				/admin 下的页面由 http-vue-loader 实现，浏览器端直接加载解析 .vue 单文件<br>
-				可打开控制台查看加载的文件
-			</li>
             <li>
-				<a href="https://www.wuxuwang.com" target="_blank">
-					打开官网
-				</a>
-			</li>
-			<li>
-				<a href="https://github.com/huanshiwushuang/wx-tpl" target="_blank">
-					github 地址
-				</a>
-			</li>
-			<li>
-				<router-link to="/index/about">
-					关于我们
-					<b>【点击查看效果】</b>
-				</router-link>
-			</li>
-			<li>
-				<a href="/">
-					前往网站根路径，查看 webpack 打包加载的页面
-				</a>
-			</li>
+                /admin 下的页面由 http-vue-loader 实现，浏览器端直接加载解析 .vue 单文件<br>
+                可打开控制台查看加载的文件
+            </li>
+            <li>
+                <a
+                    href="https://www.wuxuwang.com"
+                    target="_blank"
+                >
+                    打开官网
+                </a>
+            </li>
+            <li>
+                <a
+                    href="https://github.com/huanshiwushuang/wx-tpl"
+                    target="_blank"
+                >
+                    github 地址
+                </a>
+            </li>
+            <li>
+                <router-link to="/index/about">
+                    关于我们
+                    <b>【点击查看效果】</b>
+                </router-link>
+            </li>
+            <li>
+                <a href="/">
+                    前往网站根路径，查看 webpack 打包加载的页面
+                </a>
+            </li>
         </ul>
     </div>
 </template>
@@ -37,14 +49,21 @@
 <script>
 module.exports = {
     name: "Home",
+    async created() {
+        let res = await import(
+            "/static/admin/lib/module_httpVueLoader/config.js"
+        );
+
+        debugger;
+    },
 };
 </script>
 
 <style lang="less">
 .kseoxuv4_ul {
-	display: inline-block;
-	li {
-		padding: 10px 0;
-	}
+    display: inline-block;
+    li {
+        padding: 10px 0;
+    }
 }
 </style>
