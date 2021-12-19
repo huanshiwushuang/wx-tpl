7127b049492aff528c43e2409bb7d93cf6ff3353
guohao <gh9694@foxmail.com>
2021-12-19 18:35:35 +0800

    优化-优化优化

--- public/static/defaultApp/development/js/include_head.js
+++ public/static/defaultApp/development/js/include_head.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "25de47fe7f6318a9b3f6";
+/******/ 	var hotCurrentHash = "23ae28cbc4eb175ed33f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- wx-tpl-front/src/router/uniappHook.js
+++ wx-tpl-front/src/router/uniappHook.js
@@ -37,6 +37,8 @@ const parseTransferString = function (newTransferString) {
 }
 
 let lastTimestamp = Date.now();
+let listenerCount = 0;
+let calledCount = 0;
 
 // 初始化接收到的数据
 const initTransferString = getTransferString();
@@ -49,44 +51,51 @@ const _addEventListener = window.addEventListener;
 
 window.addEventListener = function (eventType, handler, options) {
     if (['popstate', 'hashchange'].includes(eventType)) {
+        listenerCount++;
 
         const _handler = handler;
 
         handler = function () {
+            calledCount++;
+
             const newTransferString = getTransferString();
 
             // 如果存在 transfer string
             if (newTransferString) {
-                // 如果没有 key
-                if (!history.state?.key) {
-                    // 生成 key - replace
-                    history.replaceState({
-                        key: genStateKey(),
-                    }, null, location.href);
-                }
+                // 如果是本轮触发的最后一次
+                if (calledCount % listenerCount === 0) {
+                    // 如果没有 key
+                    if (!history.state?.key) {
+                        // 生成 key - replace
+                        history.replaceState({
+                            key: genStateKey(),
+                        }, null, location.href);
+                    }
 
-                const jsonObj = parseTransferString(newTransferString);
+                    const jsonObj = parseTransferString(newTransferString);
 
-                // 如果时间戳不合适
-                if (jsonObj._timestamp > lastTimestamp) {
-                    // 保存时间戳
-                    lastTimestamp = jsonObj._timestamp;
-                    // 保存数据
-                    store.uniapp.state.receiveData = jsonObj;
-                    // 历史返回
-                    history.back();
-                }
-                // 如果正在路由
-                if (store.router.state.isRouting) {
-                    switch (store.router.state.action) {
-                        case 'push':
-                            router.push(store.router.state.to);
-                            break;
-                        case 'replace':
-                            router.replace(store.router.state.to);
-                            break;
-                        default:
-                            console.error(`store.router.state.action 错误`);
+                    // 如果时间戳不合适
+                    if (jsonObj._timestamp > lastTimestamp) {
+                        // 保存时间戳
+                        lastTimestamp = jsonObj._timestamp;
+                        // 保存数据
+                        store.uniapp.state.receiveData = jsonObj;
+                        // 历史返回
+                        history.back();
+
+                        // 如果正在路由
+                        if (store.router.state.isRouting) {
+                            switch (store.router.state.action) {
+                                case 'push':
+                                    router.push(store.router.state.to);
+                                    break;
+                                case 'replace':
+                                    router.replace(store.router.state.to);
+                                    break;
+                                default:
+                                    console.error(`store.router.state.action 错误`);
+                            }
+                        }
                     }
                 }
                 return;
